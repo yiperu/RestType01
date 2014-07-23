6fabf06d86b2a51b8e7780056fa6c6a30ff9ca42
Henry AT <yiperu@gmail.com>
2014-07-23 11:45:13 -0400

    Preparing final about Project Rest type01, parse a simple json, now prepare the server json

--- RestType01/ViewController.m
+++ RestType01/ViewController.m
@@ -18,6 +18,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //[self btnRefresh:UIButton];
+    //[self.btnRefresh sendActionForControlEvents: UIControlEventTouchUpInside];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,10 +31,50 @@
 
 - (IBAction)btnRefresh:(id)sender {
     
-    // 
+    // Esto respondera ante un JSON simple tal como:
+//    {
+//        id: 3582,
+//        content: "Hello, Sistema Sideral!"
+//    }
     
-    
-    
+    // Prepare the URL
+    NSURL *url = [NSURL URLWithString:@"http://miApiRes/greeting"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        // This block is similar to lamda or closures
+        if (data.length > 0 && connectionError == nil) {
+            NSDictionary * greeting = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+            self.lblId.text = [[greeting objectForKey:@"id"] stringValue];
+            self.lblContent.text = [greeting objectForKey:@"content"];
+        }
+    }];
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @end
